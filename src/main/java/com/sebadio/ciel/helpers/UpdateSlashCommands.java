d6999ad318d4bf3e1e16ab8130f8812e1185f5d2
sebadio <sebastiandiazdopazo@gmail.com>
2024-03-10 06:28:51 -0300

    created UpdateSlashCommands and slash commands update onReady event

---
+++ src/main/java/com/sebadio/ciel/helpers/UpdateSlashCommands.java
@@ -0,0 +1,46 @@
+package com.sebadio.ciel.helpers;
+
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+
+import java.util.ArrayList;
+
+public class UpdateSlashCommands {
+
+    public ArrayList<SlashCommandData> getCommands() {
+        return commands;
+    }
+
+    private final ArrayList<SlashCommandData> commands;
+
+    public UpdateSlashCommands() {
+        this.commands = new ArrayList<>();
+        SlashCommandData ping = Commands.slash("ping", "pong! also send the latency to the server");
+        SlashCommandData avatar = Commands.slash("avatar", "I send you the avatar of the person you want.")
+                .addOption(OptionType.USER, "user", "the user you want the avatar from", true);
+        SlashCommandData invite = Commands.slash("invite", "I send you a link to invite me to another guild!");
+        SlashCommandData userinfo = Commands.slash("userinfo", "I send you the info about a user")
+                .addOption(OptionType.USER, "user", "the user you want the info from", true);
+        SlashCommandData ban = Commands.slash("ban", "I ban the user you tell me to")
+                .addOption(OptionType.USER, "user", "The user you want to ban", true)
+                .addOption(OptionType.STRING, "reason", "The reason you want to ban them, not really important though.", false);
+        SlashCommandData unban = Commands.slash("unban", "I unban the user you tell me to")
+                .addOption(OptionType.USER, "user", "The user you want to unban", true)
+                .addOption(OptionType.STRING, "reason", "The reason you want to unban them, not really important though.", false);
+        SlashCommandData kick = Commands.slash("kick", "I kick the user you tell me to")
+                .addOption(OptionType.USER, "user", "The user you want to kick", true)
+                .addOption(OptionType.STRING, "reason", "The reason you want to kick them, not really important though.", false);
+
+        commands.add(ping);
+        commands.add(avatar);
+        commands.add(invite);
+        commands.add(userinfo);
+        commands.add(ban);
+        commands.add(unban);
+        commands.add(kick);
+    }
+
+
+
+}
--- src/main/java/com/sebadio/ciel/listeners/ReadyEventListener.java
+++ src/main/java/com/sebadio/ciel/listeners/ReadyEventListener.java
@@ -1,12 +1,37 @@
 package com.sebadio.ciel.listeners;
 
+import com.sebadio.ciel.helpers.Config;
+import com.sebadio.ciel.helpers.UpdateSlashCommands;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class ReadyEventListener extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
+        JDA jda = event.getJDA();
+        List<SlashCommandData> commandDataList = new UpdateSlashCommands().getCommands();
+
+        Guild devGuild = jda.getGuildById(Config.get().get("DEV_GUILD_ID"));
+
+        if (devGuild != null) {
+            devGuild.updateCommands().addCommands(commandDataList).queue(
+                    s -> System.out.println("Dev guild slash commands updated successfully!"),
+                    f -> System.out.println("Failed to update slash commands in dev guild.")
+            );
+        }
+
+
+        jda.updateCommands().addCommands(commandDataList).queue(
+                s -> System.out.println("Global slash commands updated successfully!"),
+                f -> System.out.println("Failed to update slash commands globally.")
+        );
+
         System.out.println("Bot Ready!");
     }
 }
