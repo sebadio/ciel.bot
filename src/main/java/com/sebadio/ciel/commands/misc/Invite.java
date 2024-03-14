a762f1928841080dd994e6d773bf51ca097a7e04
sebadio <sebastiandiazdopazo@gmail.com>
2024-03-14 11:55:23 -0300

    Invite command now asks the config for the clientid to formulate the invite

--- src/main/java/com/sebadio/ciel/commands/misc/Invite.java
+++ src/main/java/com/sebadio/ciel/commands/misc/Invite.java
@@ -9,24 +9,28 @@ import org.jetbrains.annotations.NotNull;
 
 public class Invite {
 
+    private final String inviteLink = "https://discord.com/oauth2/authorize?client_id=%s&permissions=18703474748519&scope=bot+applications.commands";
     private final String inviteMessage = "You can invite me by clicking the button";
     private final String buttonMessage = "Invite Me!";
 
     public Invite(@NotNull MessageReceivedEvent event) {
-        String inviteLink = String.format("https://discord.com/oauth2/authorize?client_id=%s&permissions=8&scope=bot+applications.commands", "1214983925626314832");
+        String CLIENT_ID  = Config.get().get("CLIENT_ID");
+        String formattedInviteLink = String.format(inviteLink, CLIENT_ID);
+
         event.getMessage()
                 .reply(inviteMessage)
                 .addActionRow(
-                        Button.link(inviteLink, buttonMessage))
+                        Button.link(formattedInviteLink, buttonMessage))
                 .queue();
     }
 
     public Invite(@NotNull SlashCommandInteractionEvent event) {
         String CLIENT_ID = Config.get().get("CLIENT_ID");
-        String inviteLink = String.format("https://discord.com/oauth2/authorize?client_id=%s&permissions=8&scope=bot+applications.commands", CLIENT_ID);
+        String formattedInviteLink = String.format(inviteLink, CLIENT_ID);
+
         event.reply(inviteMessage)
                 .addActionRow(
-                        Button.link(inviteLink, buttonMessage)
+                        Button.link(formattedInviteLink, buttonMessage)
                 ).queue();
     }
 
